bf68fcc2ab6f9c3901cbcd9b7e73f2747d780ac8
Pascal Grimaud <pascalgrimaud@users.noreply.github.com>
2018-09-05 15:34:17 +0200

    Fix git checkout

--- travis/scripts/00-install-jhipster.sh
+++ travis/scripts/00-install-jhipster.sh
@@ -62,9 +62,9 @@ else
     cd generator-jhipster
     if [ "$JHIPSTER_BRANCH" == "latest" ]; then
         LATEST=$(git describe --abbrev=0)
-        git checkout -b "$LATEST" "$LATEST"
+        git checkout "$LATEST"
     elif [ "$JHIPSTER_BRANCH" != "master" ]; then
-        git checkout -b "$JHIPSTER_BRANCH" origin/"$JHIPSTER_BRANCH"
+        git checkout "$JHIPSTER_BRANCH"
     fi
     git --no-pager log -n 10 --graph --pretty='%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit
 
